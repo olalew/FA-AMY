b1bf3075f42bf29ca03f275c918c6a4ad3f8a72b
sudokit <aleksandra.lewandowska@x-tour.pl>
2026-02-21 11:58:31 +0100

    fix

--- src/dataset/bioinformatics_dataset.py
+++ src/dataset/bioinformatics_dataset.py
@@ -2,9 +2,10 @@ import torch
 from torch.utils.data import Dataset
 
 class BioinformaticsDataset(Dataset):  #
-    def __init__(self, label, prot):
+    def __init__(self, label, prot, return_index=False):
         self.lb = label
         self.df_prot = prot
+        self.return_index = return_index
 
     def __getitem__(self, index):
         prot = self.df_prot[index]
@@ -13,6 +14,8 @@ class BioinformaticsDataset(Dataset):  #
         label = torch.tensor(label, dtype=torch.float)
         # The features extracted by ESM C have padding before and after
         data_length = 902
+        if self.return_index:
+            return prot, label, data_length, index
         return prot, label, data_length
 
     def __len__(self):
